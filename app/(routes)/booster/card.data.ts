1ec1fb30178cca48aeb995cb9b4684f8091346e8
KlayFrog <105149475+KlayFrog@users.noreply.github.com>
2024-03-02 17:01:26 +0100

    Update card.data.ts

--- app/(routes)/booster/card.data.ts
+++ app/(routes)/booster/card.data.ts
@@ -2,15 +2,7 @@ import { CardType } from "./card.model";
 
 // Instead of hard-coding the card data in the component, we can do a loop from 1 to 10
 // and generate the image path from the loop index.
-const ENERGY_CARDS: CardType[] = Array.from({ length: 10 }, (_, i) => ({
-  id: i + 1,
-  category: "energy",
-}));
-const SPECIAL_CARDS: CardType[] = Array.from({ length: 27 }, (_, i) => ({
-  category: "special",
-  id: i + 1,
-}));
-const COMMON_CARDS: CardType[] = Array.from({ length: 35 }, (_, i) => ({
+const COMMON_CARDS: CardType[] = Array.from({ length: 12 }, (_, i) => ({
   category: "common",
   id: i + 1,
 }));
